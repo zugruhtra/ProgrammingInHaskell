00d6aa1f69882f853b5b91627d0a79e1563b8abc
Arthur Guz <zugruhtra@gmail.com>
2022-08-01 12:02:45 +0200

    Removed generalized split function

--- Chapter9/countdown.hs
+++ Chapter9/countdown.hs
@@ -58,13 +58,10 @@ split []     = []
 split [_]    = []
 split (x:xs) = ([x],xs) : [(x:ls,rs) | (ls,rs) <- split xs]
 
-split' :: [a] -> [([a],[a])]
-split' xs = [([],xs)] ++ split xs ++ [(xs,[])]
-
 exprs :: [Int] -> [Expr]
 exprs [] = []
 exprs [n] = [Val n]
-exprs ns = [e | (ls,rs) <- split' ns,
+exprs ns = [e | (ls,rs) <- split ns,
                 l       <- exprs ls,
                 r       <- exprs rs,
                 e       <- combine l r]
